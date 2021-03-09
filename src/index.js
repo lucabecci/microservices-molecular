47a8ccd03e4c62db948925830a4128593c2bc5ba
luca <beccibrian@gmail.com>
2021-03-09 11:55:39 -0300

    chore(broker): Best configuration for the broker(at the moment)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const { ServiceBroker } = require("moleculer");
 
 const broker = new ServiceBroker({
+    namespace: "dev",
     nodeID: "node-1",
-    transporter: "nats://localhost:4222",
+
+    logger: true,
     logLevel: "debug",
+
+    transporter: "nats://localhost:4222",
+
     requestTimeout: 5 * 1000,
+
+    metrics: {
+        enabled: true,
+        reporter: ["Console"],
+    },
+
     errorHandler(err, info) {
         // Handle the error
         this.logger.warn("Error handled:", err);
         this.logger.warn("Error info:", info);
+        throw err;
     },
 });
