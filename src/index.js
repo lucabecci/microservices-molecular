261bda36d48e890b1197cf4e00cc06f9e973d6e6
luca <beccibrian@gmail.com>
2021-03-09 11:49:55 -0300

    chore(service): Broker added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const { ServiceBroker } = require("moleculer");
+
+const broker = new ServiceBroker({
+    nodeID: "node-1",
+    transporter: "nats://localhost:4222",
+    logLevel: "debug",
+    requestTimeout: 5 * 1000,
+    errorHandler(err, info) {
+        // Handle the error
+        this.logger.warn("Error handled:", err);
+        this.logger.warn("Error info:", info);
+    },
+});
